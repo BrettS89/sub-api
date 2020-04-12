9230c6b3fc42012dcd240719a5430113cf169ce1
BrettS89 <blsodie@gmail.com>
2020-04-12 15:24:08 -0400

    adding a console log

--- src/utils/calculateCompanyPayment.js
+++ src/utils/calculateCompanyPayment.js
@@ -1,5 +1,6 @@
-module.exports = amount => {
+module.exports = (amount) => {
 	const twelvePointNinePercent = amount * 0.129;
 	const value = (amount - twelvePointNinePercent - 0.3).toFixed(2);
+	console.log(value);
 	return value * 100;
 };
