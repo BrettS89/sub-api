591621e7b9c8e18a67de96dad1ff2524bb60275b
BrettS89 <blsodie@gmail.com>
2020-04-18 14:10:35 -0400

    changing image name

--- src/controllers/company/getUploadPhotoUrl.js
+++ src/controllers/company/getUploadPhotoUrl.js
@@ -5,7 +5,7 @@ const s3 = require('../../utils/s3');
 module.exports = async (req, res) => {
 	try {
 		const { name, type } = req.query;
-		const { url, key } = await s3.getSignedUrl(type, `${company}/${name}`);
+		const { url, key } = await s3.getSignedUrl(type, `${Date.now()}${name}`);
 		Handlers.success(res, 200, { url, key });
 	} catch (e) {
 		Handlers.error(res, e, 'getUploadPhotoUrl');
