9a03a289c921f28da3954c225fe8cf6b6be75fd2
BrettS89 <blsodie@gmail.com>
2020-05-09 12:53:29 -0400

    fixing cancel subscription

--- src/controllers/subscription/cancel.js
+++ src/controllers/subscription/cancel.js
@@ -1,3 +1,4 @@
+const Company = require('../../models/Company');
 const Subscription = require('../../models/Subscription');
 const UserSubscription = require('../../models/UserSubscription');
 const Handlers = require('../../utils/handlers');
@@ -8,11 +9,14 @@ module.exports = async (req, res) => {
 	try {
 		const { company } = await storeAuth(req.header('authorization'));
 		const { id } = req.params;
+		const foundCompany = await Company.findById(company);
+		foundCompany.subscriptions = foundCompany.subscriptions - 1;
 		const subscription = await Subscription.findById(id);
 		if (!subscription)
 			throw createError(404, 'No subscription found with this id');
 		subscription.active = false;
 		await subscription.save();
+		await foundCompany.save();
 		await UserSubscription.update(
 			{ subscription: subscription._id },
 			{ cancelledBySpot: true },
